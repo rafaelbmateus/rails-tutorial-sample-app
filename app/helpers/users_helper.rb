9deeba64ff8700f42054bec37ec3e2681a37e57c
Rafael Mateus <rafaelmateus@hotmail.com.br>
2015-10-29 19:33:48 +0000

    Correction layout microposts

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,11 +1,10 @@
 module UsersHelper
   
-  # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { :size => 50 })
+  # Returns the Gravatar (http://gravatar.com/) for the given user.
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, :alt => user.name,
-                            :class => 'gravatar',
-                            :gravatar => options)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
